a0afb90554b86f9acd59972f76ca3893e0101063
wooone <yayeahyeahya22@gmail.com>
2022-08-30 00:09:26 +0800

    create the random users section of the widgehts component

--- components/Widgets.js
+++ components/Widgets.js
@@ -2,8 +2,10 @@ import { SearchIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import News from "./News";
 
-export default function Widgets({ newsResults }) {
-  const [articleNum, setArticleNum] = useState(3)
+export default function Widgets({ newsResults, randomUsersResults }) {
+  const [articleNum, setArticleNum] = useState(3);
+  const [randomUserNum ,setRandomUserNum] = useState(3);
+
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
       <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
@@ -19,13 +21,30 @@ export default function Widgets({ newsResults }) {
 
       <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">你的流行趨勢</h4>
-        {newsResults.slice(0,articleNum).map((article) => (
+        {newsResults.slice(0, articleNum).map((article) => (
           <News key={article.title} article={article} />
         ))}
 
-		<button onClick={()=>setArticleNum(articleNum + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">顯示更多</button>
+        <button onClick={() => setArticleNum(articleNum + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">顯示更多
+        </button>
       </div>
 
+      <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+        <h4 className="font-bold text-xl px-4">跟隨誰</h4>
+        {randomUsersResults.slice(0, randomUserNum).map((randomUser) => (
+          <div key={randomUser.login.username} className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200">
+            <img className="rounded-full" width="40" src={randomUser.picture.thumbnail} alt=""/>
+            <div className="truncate ml-4 leading-5">
+              <h4 className="font-bold hover:underline text-[14px] truncate">{randomUser.login.username}</h4>
+              <h5 className="text-[13px] text-gray-500 truncate">{randomUser.name.first + " " + randomUser.name.last}</h5>
+            </div>
+            <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">跟隨</button>
+          </div>
+        ))}
+        <button onClick={()=>setRandomUserNum(randomUserNum + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
+          顯示更多
+        </button>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
 
-export default function Home({newsResults}) {
+export default function Home({ newsResults, randomUsersResults }) {
   return (
     <div>
       <Head>
@@ -19,7 +19,7 @@ export default function Home({newsResults}) {
         <Feed />
 
         {/* widgets */}
-        <Widgets newsResults={newsResults.articles}/>
+        <Widgets newsResults={newsResults.articles} randomUsersResults={randomUsersResults.results}/>
 
         {/* Modal */}
       </main>
@@ -32,9 +32,13 @@ export async function getServerSideProps() {
   const newsResults = await fetch(
     "https://newsapi.org/v2/top-headlines?country=tw&apiKey=95aa15c3cff64af9a9c2e439379bd47c"
   ).then((res) => res.json());
+
+  const randomUsersResults = await fetch("https://randomuser.me/api/?results=30&inc=name,login,picture").then((res) => res.json());
+
   return {
     props: {
       newsResults,
+      randomUsersResults,
     },
   };
 }
