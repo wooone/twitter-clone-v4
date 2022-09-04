60066bfe1279bc84ba3dd494f84b9216453027f0
wooone <yayeahyeahya22@gmail.com>
2022-09-04 19:25:13 +0800

    install and implement recoil

--- components/Post.js
+++ components/Post.js
@@ -20,11 +20,14 @@ import { db, storage } from "../firebase";
 import { signIn, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { deleteObject, ref } from "firebase/storage";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atom/modalAtom";
 
 export default function Post({ post }) {
   const { data: session } = useSession();
   const [likes, setLikes] = useState([]);
   const [hasliked, setHasLiked] = useState(false);
+  const [open, setOpen] = useRecoilState(modalState);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -112,7 +115,7 @@ export default function Post({ post }) {
         {/* icons */}
 
         <div className="flex justify-between text-gray-500 p-2 ">
-          <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <ChatIcon onClick={()=>setOpen(!open)} className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           {session?.user.uid === post?.data().id && (
             <TrashIcon
               onClick={deletePost}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import CommentModal from "../components/CommentModal";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
@@ -22,6 +23,7 @@ export default function Home({ newsResults, randomUsersResults }) {
         <Widgets newsResults={newsResults.articles} randomUsersResults={randomUsersResults.results}/>
 
         {/* Modal */}
+        <CommentModal />
       </main>
     </div>
   );
